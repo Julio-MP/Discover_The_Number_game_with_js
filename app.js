9b1eec3f00bafe1f96ec22c18f5928614d08abea
Julio Moreno Possebon <julio.possebon1@gmail.com>
2024-08-06 23:33:23 -0300

    EndOfDayCommit(2)

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let secretNumber = ramdomNumber();
+let attempts = 1
 
 
 //Aqui a função escreve a mensagem do criador do jogo na tela, mas ela não armazena nada, por isso usamos um parametro dentro da função (pra escrever o texto que a gente quer que ele seja escrito) mas não usou return porque não queremos que ele salve nada (o texto que ele escreveu ficou "perdido" na memoria do programa, mas ta na tela que é isso que importa)
@@ -14,20 +15,27 @@ rightThings('p', 'Choose a number between 1 and 100')
 
 // Aqui a gente quer qeu ele execute uma função mais automata 
 function verifytheGuess() {
-    let theGuess = document.querySelector('input').value
+    let theGuess = document.querySelector('input').value;
     if (theGuess == secretNumber) {
-        alert('You Win!')
+        rightThings('h1', 'You Win!');
+        rightThings('p', `Number of attempts: ${attempts}`);
+        cleanInput()
+        
     
     }
     
     else {
         if (theGuess > secretNumber) {
-            alert(`It's lower than ${theGuess}`)
+            rightThings('p', `It's lower than ${theGuess}`);
+            
         }
 
         else {
-            alert(`It's bigger than ${theGuess}`)
+            rightThings('p', `It's bigger than ${theGuess}`);
+            
         }
+        attempts++
+        cleanInput()
     }
     
 }
@@ -35,6 +43,15 @@ function verifytheGuess() {
 //Aqui a gente quer que ele gere um numero pra gente usa-lo no sistema depois, então temos que salvar esse numero, e pra isso a gente usa o return 
 function ramdomNumber() {
     return parseInt(Math.random() *100 +1)
+}
+
+function cleanInput() {
+    Guess = document.querySelector('input')
+    Guess.value = ''
+}
+
+function restart() {
+    location.reload()
 }
 
 console.log(secretNumber)
